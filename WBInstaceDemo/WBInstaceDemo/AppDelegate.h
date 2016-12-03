4585c5320d26ae51dda0b4ea587a2edf117642d0
lsrain <315192042@qq.com>
2016-12-04 03:14:30 +0800

    project init

---
+++ WBInstaceDemo/WBInstaceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBInstaceDemo
+//
+//  Created by WangBiao on 2016/12/4.
+//  Copyright © 2016年 lsrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WBInstaceDemo/WBInstaceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBInstaceDemo
+//
+//  Created by WangBiao on 2016/12/4.
+//  Copyright © 2016年 lsrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
